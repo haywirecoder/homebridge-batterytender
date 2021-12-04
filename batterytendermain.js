909e6cad087053436965cbb2e402a0345d6057a0
haywirecoder <44388297+haywirecoder@users.noreply.github.com>
2021-12-04 12:31:35 -0500

    Add files via upload

---
+++ batterytendermain.js
@@ -0,0 +1,134 @@
+
+const EventEmitter = require('events');
+const needle = require('needle');
+
+// URL constant for retrieving data
+
+const HOST = 'https://balanced.deltranconnected.com'
+const LOGIN_PATH = '/api/users/login'
+const AUTHENTICATE_PATH = '/api/users/authenticate'
+const CHARGER_PATH = '/api/charger/status'
+const MONITOR_PATH = '/api/monitor/status'
+
+class BatteryTender extends EventEmitter {
+    auth_token = {};
+    batteryTenderDevicesMonitors = [];
+    excludedDevices = []
+    log;
+    deviceRefreshHandle;
+    deviceRefreshTime;
+    debug;
+
+
+    constructor(log, config) {
+        super();
+        this.log = log;
+        this.deviceRefreshHandle = null;
+        this.deviceRefreshTime = config.deviceRefresh * 3600000 || 3600000;
+        this.excludedDevices = config.excludedDevices || [];
+        this.auth_token.email = config.auth.email;
+        this.auth_token.password = config.auth.password;
+        this.auth_token.token ="";
+    };
+
+    // Initial login and building of device list 
+    async init() {
+
+        this.log.info("Battery Tender Login...");
+        var url = HOST + LOGIN_PATH;
+        
+        try {
+            const response = await needle("post", url,
+            {
+                email: this.auth_token.email,
+                password: this.auth_token.password
+
+            });
+                // this.log.info(response);
+            var device_response = response;
+            this.log.debug("Battery Raw Data: ", device_response.body,  "\n");
+            this.auth_token.token = device_response.body.token;
+            // Discovery and create device list
+            for(var i in device_response.body.monitors)
+            {
+                if (this.excludedDevices.includes(device_response.body.monitors[i]['name'])) {
+                    this.log.info(`Executing Device with name: '${device_response.body.monitors[i]['name']}'`);
+                    
+                } else {
+                    var device = {};
+                    device.deviceId = device_response.body.monitors[i]['deviceId'];
+                    device.name = device_response.body.monitors[i]['name'];
+                    device.lastUpdate = new Date(device_response.body.monitors[i]['statusHistory'][0]['date']);
+                    device.soc = device_response.body.monitors[i]['statusHistory'][0]['soc'];
+                    device.voltage = device_response.body.monitors[i]['statusHistory'][0]['voltage'];
+                    this.batteryTenderDevicesMonitors.push(device);
+                }
+
+            }
+            this.log.debug("Device dump: ", this.batteryTenderDevicesMonitors, "\n");
+        }
+        catch(err) {
+            // Something went wrong, display message and return negative return code
+            this.log.error("Battery Monitoring Device Login Error: ", err.message);
+            return false;
+        } 
+    }
+
+    // Start for periodic refresh of devices
+    startPollingProcess()
+    {
+        // Set time to refresh devices
+       this.deviceRefreshHandle = setTimeout(() => this.backgroundRefresh(), this.deviceRefreshTime); 
+     
+    };
+
+    async backgroundRefresh() {
+
+        this.log("Battery Monitor Device Refresh...");
+        var url = HOST + MONITOR_PATH;
+        
+        try {
+            const response = await needle("get", url,
+            {
+                token: this.auth_token.token
+
+            });
+            var device_response = response;
+            this.log(device_response);
+            this.log.debug("Battery Raw Data: ", device_response.body,  "\n");
+            var deviceUpdateDate;
+            for(var i in device_response.body.monitors)
+            {
+                // find device and determine if it needs to be updated.
+                var findIndex = this.batteryTenderDevicesMonitors.findIndex(device => device.deviceId === device_response.body.monitors[i]['deviceId']);
+                
+                if (findIndex > -1)
+                { 
+                    this.log.debug(`Found device at index: ${findIndex}`);
+                    deviceUpdateDate = new Date(device_response.body.monitors[i]['statusHistory'][0]['date']+30);
+                    if (this.batteryTenderDevicesMonitors[findIndex].lastUpdate.getTime() != deviceUpdateDate.getTime())
+                    {
+                        this.batteryTenderDevicesMonitors[findIndex].lastUpdate = new Date(device_response.body.monitors[i]['statusHistory'][0]['date']);
+                        this.batteryTenderDevicesMonitors[findIndex].voltage = device_response.body.monitors[i]['statusHistory'][0]['voltage'];
+                        this.batteryTenderDevicesMonitors[findIndex].soc = device_response.body.monitors[i]['statusHistory'][0]['soc'];
+                        // Device data has changed, emit and event to process 
+                        this.log.debug("Battery Device Update Triggered: ", this.batteryTenderDevicesMonitors[findIndex]);
+                        this.emit(this.batteryTenderDevicesMonitors[findIndex].deviceid, {
+                            device: this.batteryTenderDevicesMonitors[findIndex]
+                        });
+                   }
+                } else this.log.debug(`Device not found: ${device_response.body.monitors[i]['deviceId']}`);
+            }
+            
+        }
+        catch(err) {
+            // Something went wrong, display message and return negative return code
+            this.log.error("Battery Monitoring Device Refresh Error: ", err.message);
+            return false;
+        } ;
+        // this.deviceRefreshHandle = setTimeout(() => this.backgroundRefresh(), this.deviceRefreshTime); 
+    }
+
+}
+          
+module.exports = BatteryTender;
---
+++ index.js
@@ -0,0 +1,162 @@
+"use strict";
+const btengine = require('./batterytendermain');
+const voltageguage = require('./accessories/voltsensor');
+
+const PLUGIN_NAME = 'homebridge-batterytender';
+const PLATFORM_NAME = 'BatteryTender';
+
+
+var Service, Characteristic, HomebridgeAPI, UUIDGen;
+
+module.exports = function(homebridge) {
+
+  Service = homebridge.hap.Service;
+  Characteristic = homebridge.hap.Characteristic;
+  HomebridgeAPI = homebridge;
+  UUIDGen = homebridge.hap.uuid;
+  homebridge.registerPlatform(PLUGIN_NAME, PLATFORM_NAME, BatteryTenderPlatform);
+}
+
+class BatteryTenderPlatform {
+  constructor(log, config, api) {
+  this.log = log;
+  this.api = api;
+  this.name = config.name;
+  this.accessories = [];
+  
+  // Check if authentication information has been provided.
+  /*try{
+    if ((this.config.auth.email == "") || (this.config.auth.password == ""))
+    {
+      this.log.error('Plug-in configuration error: Flo authentication information not provided.');
+      // terminate plug-in initization
+      return;
+    }
+  }
+  catch(err) {
+    this.log.error('Plug-in configuration error: Flo authentication information not provided.');
+    // terminate plug-in initization
+    return;
+  }*/
+
+  this.bt = new btengine (log, config);
+ 
+  // When this event is fired it means Homebridge has restored all cached accessories from disk.
+  // Dynamic Platform plugins should only register new accessories after this event was fired,
+  // in order to ensure they weren't added to homebridge already. This event can also be used
+  // to start discovery of new accessories.
+  api.on('didFinishLaunching', () => {
+
+    this.initialLoad =  this.bt.init().then (() => {
+       this.log.debug('Initialization Successful.');
+       // Once devices are discovered update Homekit assessories
+       this.refreshAccessories();
+    }).catch(err => {
+      this.log.error('BatterTender Initization Failure:', err);
+      // terminate plug-in initization
+      return;
+    });
+    
+  });
+  }
+   // Create associates in Homekit based on devices in BatteryTender account
+  async refreshAccessories() {
+  
+    // Process each flo devices and create accessories within the platform.
+    this.log.info("Loading Devices...");
+    for (var i = 0; i < this.bt.batteryTenderDevicesMonitors.length; i++) {
+
+      let currentDevice = this.bt.batteryTenderDevicesMonitors[i];
+      this.log.debug("Processing Device", currentDevice);
+      var sensorAccessory = new voltageguage(this.bt, currentDevice, this.config, this.log, Service, Characteristic, UUIDGen);
+      // check the accessory was not restored from cache
+      var foundAccessory = this.accessories.find(accessory => accessory.UUID === sensorAccessory.uuid)
+      if (!foundAccessory) {
+        // create a new accessory
+        let newAccessory = new this.api.platformAccessory(sensorAccessory.name, sensorAccessory.uuid);
+        // add services and Characteristic
+        sensorAccessory.setAccessory(newAccessory);
+        // register the accessory
+        this.addAccessory(sensorAccessory);
+      }
+      else // accessory already exist just set characteristic
+        sensorAccessory.setAccessory(foundAccessory);
+    }
+    this.log.info(`Monitoring Devices configured: ${this.bt.batteryTenderDevicesMonitors.length}`);
+
+     // Clean accessories with no association with Flo devices.
+     this.orphanAccessory();
+     // Start background process to poll devices.
+     this.bt.startPollingProcess();
+  };
+
+// Find accessory with no association with battery monitoring device and remove
+async orphanAccessory() {
+  var cachedAccessory = this.accessories;
+  var foundAccessory;
+
+  for (var i = 0; i < cachedAccessory.length; i++) 
+  {   
+    let accessory = cachedAccessory[i];
+    // determine if accessory is currently a device in flo system, thus should remain
+    foundAccessory = this.bt.batteryTenderDevicesMonitors.find(device => UUIDGen.generate(device.deviceId.toString()) === accessory.UUID)
+    if (!foundAccessory) {
+      // determine if an optional compoment, thus should remain
+      foundAccessory = this.optionalAccessories.find(optionalAccessory => optionalAccessory.uuid === accessory.UUID);
+      if (!foundAccessory) {
+          this.removeAccessory(accessory,true);
+      }
+    }
+  }
+}
+
+//Add accessory to homekit dashboard
+addAccessory(device) {
+
+  this.log.info('Adding accessory');
+      try {
+        this.api.registerPlatformAccessories(PLUGIN_NAME, PLATFORM_NAME, [device.accessory]);
+        this.accessories.push(device.accessory);
+      } catch (err) {
+          this.log.error(`An error occurred while adding accessory: ${err}`);
+      }
+}
+
+//Remove accessory to homekit dashboard
+removeAccessory(accessory, updateIndex) {
+  this.log.info('Removing accessory:',accessory.displayName );
+    if (accessory) {
+        this.api.unregisterPlatformAccessories(PLUGIN_NAME, PLATFORM_NAME, [accessory]);
+    }
+    if (updateIndex) {
+      if (this.accessories.indexOf(accessory) > -1) {
+          this.accessories.splice(this.accessories.indexOf(accessory), 1);
+    }}
+  }
+
+  // This function is invoked when homebridge restores cached accessories from disk at startup.
+  // It should be used to setup event handlers for characteristics and update respective values.
+  configureAccessory(accessory) {
+    this.log.info('Loading accessory from cache:', accessory.displayName);
+    // add the restored accessory to the accessories cache so we can track if it has already been registered
+    this.accessories.push(accessory);
+  } 
+  
+  ex_getService(name) {
+    if (name instanceof Service)
+        return name;
+
+    for (var index in this.services) {
+        var service = this.services[index];
+        
+        if (typeof name === 'string' && (service.displayName === name || service.name === name))
+            return service;
+        else if (typeof name === 'function' && ((service instanceof name) || (name.UUID === service.UUID)))
+            return service;
+      }
+    
+}
+
+
+
+}
