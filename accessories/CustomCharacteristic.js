8b291fdc89f0e3ca6ede97f1578aeebd516ebad3
haywirecoder <44388297+haywirecoder@users.noreply.github.com>
2022-08-26 01:49:03 -0400

    Add files via upload

---
+++ accessories/CustomCharacteristic.js
@@ -0,0 +1,109 @@
+const WATTS = 'E863F10D-079E-48FF-8F27-9C2605A29F52';
+const TOTALWATTS = 'E863F10C-079E-48FF-8F27-9C2605A29F52';
+const VOLTAGE = 'E863F10A-079E-48FF-8F27-9C2605A29F52';
+const AMPERE = 'E863F126-079E-48FF-8F27-9C2605A29F52';
+const POWERMETER  = '00000001-0000-1777-8000-775D67EC4377';
+
+Wattage = function (Homebridge) {
+  Service = Homebridge.hap.Service;
+  Characteristic = Homebridge.hap.Characteristic;
+
+  return class EvePowerConsumption extends Characteristic {
+  static UUID = WATTS;
+  constructor() {
+    super('Consumption', WATTS);
+    this.setProps({
+      format: Characteristic.Formats.UINT16,
+      unit: "Watts",
+      maxValue: 100000,
+      minValue: 0,
+      minStep: 1,
+      perms: [Characteristic.Perms.READ, Characteristic.Perms.NOTIFY]
+    });
+    this.value = this.getDefaultValue();
+    }   
+  }
+}
+
+TotalConsumption = function (Homebridge) {
+  Service = Homebridge.hap.Service;
+  Characteristic = Homebridge.hap.Characteristic;
+
+  return class EveTotalConsumption extends Characteristic {
+    static UUID = TOTALWATTS;
+    constructor() {
+      super('Energy', TOTALWATTS);
+      this.setProps({
+        format: Characteristic.Formats.FLOAT,
+        unit: 'kWh',
+        maxValue: 1000000000,
+        minValue: 0,
+        minStep: 0.001,
+        perms: [Characteristic.Perms.READ, Characteristic.Perms.NOTIFY]
+      });
+      this.value = this.getDefaultValue();
+    }
+  }
+}
+
+Voltage = function (Homebridge) {
+  Service = Homebridge.hap.Service;
+  Characteristic = Homebridge.hap.Characteristic;
+
+  return class EveVoltage extends Characteristic  {
+    static UUID = VOLTAGE;
+    constructor() {
+      super ('Volt', VOLTAGE);
+      this.setProps({
+        format: Characteristic.Formats.FLOAT,
+        unit: 'Volt',
+        maxValue: 1000000000,
+        minValue: 0,
+        minStep: 0.001,
+        perms: [Characteristic.Perms.READ, Characteristic.Perms.NOTIFY]
+      });
+      this.value = this.getDefaultValue();
+    }
+  }
+}
+
+Meter = function (Homebridge) {
+  Service = Homebridge.hap.Service;
+  Characteristic = Homebridge.hap.Characteristic;
+
+  return class EveAmpere extends Characteristic {
+    static UUID = AMPERE;
+    constructor() {
+      super('Ampere', AMPERE);
+      this.setProps({
+        format: Characteristic.Formats.FLOAT,
+        unit: 'Ampere',
+        maxValue: 1000000000,
+        minValue: 0,
+        minStep: 0.001,
+        perms: [Characteristic.Perms.READ, Characteristic.Perms.NOTIFY]
+      });
+      this.value = this.getDefaultValue();
+    }
+  }
+}
+
+PowerMonitorService = function (Homebridge) {
+  Service = Homebridge.hap.Service;
+  Characteristic = Homebridge.hap.Characteristic;
+
+  return class PowerMeterService extends Service {
+    static UUID  = POWERMETER;
+    constructor (displayName, subtype) {
+      super(displayName, POWERMETER, subtype);
+      this.addCharacteristic(Voltage(Homebridge))
+      this.addOptionalCharacteristic(Wattage(Homebridge));
+      this.addOptionalCharacteristic(TotalConsumption(Homebridge));
+      this.addOptionalCharacteristic(Meter(Homebridge));
+    };
+  }
+}
+
+module.exports= {PowerMonitorService,Meter,Voltage,TotalConsumption,Wattage}
+
+
--- accessories/voltsensor.js
+++ accessories/voltsensor.js
@@ -1,26 +1,30 @@
 "use strict";
-const MIN_LUX_VALUE = 1.0;
-const MAX_LUX_VALUE = 13.0;
+const EvePower = require('./CustomCharacteristic');
+const EvePowerMonitoringService = EvePower.PowerMonitorService;
+const EveVoltage = EvePower.Voltage;
 
 class voltageGuageSensor {
-    constructor(bt, device, config, log, Service, Characteristic, UUIDGen) {
+    constructor(bt, device, config, log, Service, Characteristic, UUIDGen, Homebridge) {
     this.Characteristic = Characteristic;
     this.Service = Service;
     this.name = device.name;
     this.id = device.deviceId.toString();
     this.log = log;
-    this.voltage = device.voltage; 
+    this.devicevoltage = device.voltage; 
     this.soc = device.soc;
-    this.minSensorValue = MIN_LUX_VALUE;
-    this.maxSensorValue = MAX_LUX_VALUE;
     this.uuid = UUIDGen.generate(this.id);
     this.bt = bt;
     this.bt.on(this.id, this.refreshState.bind(this));
+    this.powerMonitor = EvePowerMonitoringService(Homebridge);
+    this.voltage = EveVoltage(Homebridge);
+    // Create voltage characteristic for easy reference
+    this.Characteristic = Object.defineProperty(Homebridge.hap.Characteristic, 'volt', {value: this.voltage});
   }
+
   refreshState(eventData)
   {
     this.log.debug(`Device updated requested: ` , eventData);
-    this.voltage = eventData.device.voltage|| 1;
+    this.devicevoltage = eventData.device.voltage|| 1;
     this.soc = eventData.device.soc || 0;
   }
 
@@ -29,55 +33,66 @@ class voltageGuageSensor {
     this.accessory.getService(this.Service.AccessoryInformation)
         .setCharacteristic(this.Characteristic.Manufacturer, 'Deltan')
         .setCharacteristic(this.Characteristic.SerialNumber, this.id);
+    
+    var outletService = this.accessory.getService(this.Service.Outlet);
+    if(outletService == undefined) outletService = this.accessory.addService(this.Service.Outlet,this.name); 
+    outletService.setCharacteristic(this.Characteristic.Name, this.name); 
+    outletService.getCharacteristic(this.Characteristic.On)
+          .on("get",  async callback => this.getPowerState(callback));
+			
+    var powerMeterService = this.accessory.getService(this.powerMonitor);
+    if (powerMeterService == undefined) {
+        powerMeterService = this.accessory.addService(this.powerMonitor, "Power Levels"); 
+        outletService.addLinkedService(powerMeterService);
+        this.accessory.context.powerMeterService = powerMeterService;
+    }
+    powerMeterService.getCharacteristic(this.Characteristic.volt) 
+       .on("get",  async callback => this.getVolt(callback));
 
-    var lightSensor = this.accessory.getService(this.Service.LightSensor);
-    if(lightSensor == undefined) lightSensor = this.accessory.addService(this.Service.LightSensor,this.name); 
-    lightSensor.setCharacteristic(this.Characteristic.Name, this.name); 
-    lightSensor.getCharacteristic(this.Characteristic.CurrentAmbientLightLevel)
-      .on('get', async callback => this.getSensorValue(callback))
-      .setProps({
-        minValue: this.minSensorValue,
-        maxValue: this.maxSensorValue
-      });
-
+    
     var batteryService = this.accessory.getService(this.Service.Battery);
-    if(batteryService == undefined) batteryService = this.accessory.addService(this.Service.Battery, "Battery Level"); 
+    if(batteryService == undefined) {
+      batteryService = this.accessory.addService(this.Service.Battery, "Battery Level"); 
+      outletService.addLinkedService(batteryService);
+    }
     // create handlers for required characteristics
     batteryService.getCharacteristic(this.Characteristic.StatusLowBattery)
       .on('get', async callback => this.getStatusLowBattery(callback));
     batteryService.getCharacteristic(this.Characteristic.BatteryLevel)
       .on('get', async callback => this.getBatteryLevel(callback));
-    lightSensor.addLinkedService(batteryService);
-  
+
+
   }
 
   // Handle requests to get the current value of the "Current Ambient Light Level" characteristic
-  async getSensorValue(callback) {
-    this.log.debug('Triggered GET SensorValue');
-    // set this to a valid value for CurrentAmbientLightLevel
-    const currentValue =  this.voltage;
-    this.log.debug('Return', currentValue);
+  async getPowerState(callback) {
+    // set this to a valid value for On
+    const currentValue = 0;
     return callback(null, currentValue);
   }
   // Handle requests to get the current value of the "Status Low Battery" characteristic
    
   async getStatusLowBattery(callback) {
-    this.log.debug('Triggered GET StatusLowBattery');
-
     // set this to a valid value for StatusLowBattery
     var currentValue = this.Characteristic.StatusLowBattery.BATTERY_LEVEL_NORMAL;
     if (this.soc < 20) currentValue = this.Characteristic.StatusLowBattery.BATTERY_LEVEL_LOW;
-    this.log.debug('Return', currentValue);
+    this.log.debug('getStatusLowBattery  StatusLowBattery - ', currentValue);
     return callback(null, currentValue);
   }
 
   async getBatteryLevel(callback) {
-    this.log.debug('Triggered GET BatteryLevel');
+    this.log.debug('getBatteryLevel: BatteryLevel - ', this.soc);
     // set this to current battery level
-    const currentValue =  this.soc;
-    this.log.debug('Return', currentValue);
+    const currentValue =  this.soc
     return callback(null, currentValue);
   }
-}
 
+  async getVolt(callback) {
+    this.log.debug('getVolt: Voltage - ', this.devicevoltag);
+    // set this to current battery level
+    const currentValue =  this.devicevoltage;
+    return callback(null, currentValue);
+  }
+  
+}
 module.exports = voltageGuageSensor;
